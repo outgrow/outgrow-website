16ed73679397fbb4f33a88e793ee728bbae9f884
Loan Laux <loan@outgrow.io>
2018-12-25 19:37:27 +0100

    Add animated hamburger button and menu toggling

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { Component }  from "react"
 import Link from "next/link"
 import styled from "styled-components"
+import classNames from "classnames"
 
 const links = [
   { href: "/training", label: "Developer Training" },
@@ -18,15 +19,107 @@ const TopBar = styled.div`
   top: 0;
   width: 100%;
   z-index: 999;
+
+  align-items: end;
+  display: flex;
+  justify-content: space-between;
+
+  background: rgba(0, 0, 0, .4);
 `
 
-const MenuButton = styled.i`
+const MenuButton = styled.button`
+  padding: 15px 15px;
+  display: inline-block;
+  cursor: pointer;
+  transition-property: opacity, filter;
+  transition-duration: 0.15s;
+  transition-timing-function: linear;
+  font: inherit;
+  color: inherit;
+  text-transform: none;
+  background-color: transparent;
+  border: 0;
+  margin: 0;
+  overflow: visible;
+  
+  &:hover {
+    opacity: 0.7;
+  }
+  
+  &.is-active:hover {
+    opacity: 0.7;
+  }
+  
+  &.is-active .hamburger-inner,
+  &.is-active .hamburger-inner::before,
+  &.is-active .hamburger-inner::after {
+    background-color: #fff;
+  }
 
+  &.is-active .hamburger-inner {
+    transition-delay: 0.22s;
+    background-color: transparent !important;
+  }
+
+  &.is-active .hamburger-inner::before {
+    top: 0;
+    transition: top 0.1s 0.15s cubic-bezier(0.33333, 0, 0.66667, 0.33333), transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 10px, 0) rotate(45deg);
+  }
+
+  &.is-active .hamburger-inner::after {
+    top: 0;
+    transition: top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333), transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+    transform: translate3d(0, 10px, 0) rotate(-45deg);
+  }
+`
+
+const MenuButtonBox = styled.span`
+  width: 40px;
+  height: 24px;
+  display: inline-block;
+  position: relative;
+`
+
+const MenuButtonInner = styled.span`
+  display: block;
+  margin-top: -2px;
+  top: 2px;
+  transition: background-color 0s 0.13s linear;
+
+  &,
+  &::before,
+  &::after {
+    width: 40px;
+    height: 2px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    transition-property: transform;
+    transition-duration: 0.15s;
+    transition-timing-function: ease;
+  }
+
+  &::before,
+  &::after {
+    content: "";
+    display: block;
+  }
+
+  &::before {
+    top: 10px;
+    transition: top 0.1s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1), transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
+
+  &::after {
+    bottom: -10px;
+    top: 20px;
+    transition: top 0.2s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1), transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+  }
 `
 
 const Logo = styled.img`
-  height: 2rem;
-  float: right;
+  height: 2.5rem;
   margin: .5rem .5rem 0 0;
 `
 
@@ -94,35 +187,63 @@ const ButtonLink = styled.a`
   }
 `
 
-const Nav = () => (
-  <div>
-    <TopBar>
-      <MenuButton />
-      <Logo src="/static/logo-white.svg" />
-    </TopBar>
-    <MobileNavWrapper>
-      <List>
-        {links.map(({ key, href, label }) => (
-          <ListItem key={key}>
-            <Link href={href}>
-              <ListItemLink>{label}</ListItemLink>
-            </Link>
-          </ListItem>
-        ))}
+class Nav extends Component {
+  constructor() {
+    super();
 
-        <ListItem>
-          <Divider />
-          <DividerText>or</DividerText>
-        </ListItem>
+    this.state = {
+      isMenuOpen: false
+    }
+  }
 
-        <ListItem>E-Commerce Site Down?</ListItem>
-        <ButtonLink href="tel:+18009680818">
-          (800) 968-0818<br/>
-          <span>24/7 Emergency Hotline</span>
-        </ButtonLink>
-      </List>
-    </MobileNavWrapper>
-  </div>
-)
+  handleToggleMenu = () => this.setState({
+    isMenuOpen: !this.state.isMenuOpen
+  })
+
+  render() {
+    return (
+      <div>
+        <TopBar>
+          <MenuButton
+            className={classNames({
+              "hamburger": true,
+              "hamburger--spring": true,
+              "is-active": this.state.isMenuOpen
+            })}
+            type="button"
+            onClick={this.handleToggleMenu}
+          >
+            <MenuButtonBox className="hamburger-box">
+              <MenuButtonInner className="hamburger-inner" />
+            </MenuButtonBox>
+          </MenuButton>
+          <Logo src="/static/logo-white.svg" />
+        </TopBar>
+        {this.state.isMenuOpen && <MobileNavWrapper>
+          <List>
+            {links.map(({ key, href, label }) => (
+              <ListItem key={key}>
+                <Link href={href}>
+                  <ListItemLink>{label}</ListItemLink>
+                </Link>
+              </ListItem>
+            ))}
+
+            <ListItem>
+              <Divider />
+              <DividerText>or</DividerText>
+            </ListItem>
+
+            <ListItem>E-Commerce Site Down?</ListItem>
+            <ButtonLink href="tel:+18009680818">
+              (800) 968-0818<br/>
+              <span>24/7 Emergency Hotline</span>
+            </ButtonLink>
+          </List>
+        </MobileNavWrapper>}
+      </div>
+    )
+  }
+}
 
 export default Nav
