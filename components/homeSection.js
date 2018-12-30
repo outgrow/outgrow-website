a8ec4d9e9554dd902cdd117bbf3a1a7884f5c39d
Loan Laux <loan@outgrow.io>
2018-12-30 19:41:00 +0100

    Isolate home section into its own component

---
+++ components/homeSection.js
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+import Button from "../components/button"
+
+const Overlay = styled.section`
+  height: 100%;
+  width: 100%;
+  padding: 0 1.2rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+`
+
+const SectionWithBackgroundImage = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: 50%;
+`
+
+const Title = styled.h2`
+  color: white;
+  font-size: 2.4rem;
+  font-weight: 800;
+  letter-spacing: -2px;
+`
+
+const HomeSection = ({ title, buttonText, buttonHref }) => (
+  <SectionWithBackgroundImage style={{ backgroundImage: "url(/static/consulting.jpg)" }}>
+    <Overlay style={{ backgroundColor: "rgba(51, 151, 246, .57)" }}>
+      <Title>{title}</Title>
+      <Button href={buttonHref}>{buttonText}</Button>
+    </Overlay>
+  </SectionWithBackgroundImage>
+)
+
+export default HomeSection
+
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,20 @@
 import React from "react"
-import Link from "next/link"
-import styled from "styled-components"
 import Head from "../components/head"
 import Nav from "../components/nav"
-import Button from "../components/button"
-
-const Overlay = styled.section`
-  height: 100%;
-  width: 100%;
-  padding: 0 1.2rem;
-  display: flex;
-  align-items: flex-start;
-  justify-content: center;
-  flex-direction: column;
-`
-
-const SectionWithBackgroundImage = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-size: cover;
-  background-position: 50%;
-`
-
-const Title = styled.h2`
-  color: white;
-  font-size: 2.4rem;
-  font-weight: 800;
-  letter-spacing: -2px;
-`
+import HomeSection from "../components/homeSection"
 
 const Home = () => (
   <div>
     <Head title="E-Commerce Consulting & Emergency Response | out:grow" />
     <Nav />
-     
-    <SectionWithBackgroundImage style={{ backgroundImage: "url(/static/consulting.jpg)" }}>
-      <Overlay style={{ backgroundColor: "rgba(51, 151, 246, .57)" }}>
-        <Title>Redefining <br/>e-commerce <br/>consulting.</Title>
-        <Button href="/consulting">Learn more</Button>
-      </Overlay>
-    </SectionWithBackgroundImage>
+    
+    <HomeSection
+      buttonHref="/consulting"
+      buttonText="Learn more"
+      title="Redefining e-commerce consulting."
+    /> 
   </div>
 )
 
 export default Home
+
