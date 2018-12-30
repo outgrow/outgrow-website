5061fb01e8f4457957124caf74038c9722b5974b
Loan Laux <loan@outgrow.io>
2018-12-30 16:58:32 +0100

    Add first home section

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
+import React from "react"
+import Link from "next/link"
+import styled from "styled-components"
+import Head from "../components/head"
+import Nav from "../components/nav"
+
+const Section = styled.section`
+  height: 100vh;
+  width: 100vw;
+  background-size: cover;
+  background-position: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  backdrop-filter: rgba(1, 111, 185, 0.4);
+`
+
+const Title = styled.h2`
+  color: white;
+`
 
 const Home = () => (
   <div>
     <Head title="E-Commerce Consulting & Emergency Response | out:grow" />
     <Nav />
-
+     
+    <Section style={{ backgroundImage: "url(/static/consulting.jpg)" }}>
+      <Title>Redefining e-commerce consulting.</Title>
+    </Section>
   </div>
 )
 
