559fd400c63adf1582534f61f960a18455c1544e
Loan Laux <loan@outgrow.io>
2018-12-30 20:32:16 +0100

    Add consulting page

---
+++ pages/ecommerce-consulting.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Head from "../components/head"
+import Nav from "../components/nav"
+
+const Consulting = () => (
+  <div>
+    <Head title="E-Commerce Consulting & Emergency Response | out:grow" />
+    <Nav />
+
+    <h1>Hello World!</h1>
+  </div>
+)
+
+export default Consulting
+
