c8cd3a4272c49054382d70c2114d0a86fcfcf63a
Loan Laux <loan@outgrow.io>
2019-01-08 16:05:41 +0100

    Isolate CSS in styles directory

--- components/head.js
+++ components/head.js
@@ -1,7 +1,7 @@
 import React from "react"
 import NextHead from "next/head"
 import { string } from "prop-types"
-import "./fonts.css"
+import "../styles/fonts.css"
 
 const defaultDescription = ""
 const defaultOGURL = ""
