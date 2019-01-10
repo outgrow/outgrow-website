69d72c9068c6b5aeea87137aa70ff8fb33601351
Loan Laux <loan@outgrow.io>
2019-01-10 13:17:34 +0100

    Re-export defaults from component index

---
+++ components/index.js
@@ -0,0 +1,8 @@
+export { default as BulletPoint } from "./bulletPoint"
+export { default as Button } from "./button"
+export { default as Head } from "./head"
+export { default as HomeSection } from "./homeSection"
+export { default as Nav } from "./nav"
+export { default as ServicePageHeader } from "./servicePageHeader"
+export { default as Wire } from "./wire"
+
