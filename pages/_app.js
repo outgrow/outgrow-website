7ac87ea54b4687c7afdf1b0752916f1c83978c43
Loan Laux <loan@outgrow.io>
2019-01-27 16:44:14 +0100

    Use react-fullstory instead of snippet

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+import React from "react"
 import App from "next/app"
 import Router from "next/router"
 import withGA from "next-ga"
+import FullStory from "react-fullstory"
 
-export default withGA("UA-132569116-1", Router)(App);
+const CustomApp = (props) => ([
+  <App {...props} />,
+  <FullStory org="J1X09" />
+])
+
+export default withGA("UA-132569116-1", Router)(CustomApp);
 
