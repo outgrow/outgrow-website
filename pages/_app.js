1410742e19b3e9ea6f88f0b834153513c9f59d3c
Loan Laux <loan@outgrow.io>
2021-01-22 13:37:24 +0400

    remove FullStory
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import React from "react"
 import App from "next/app"
 import Router from "next/router"
 import withGA from "next-ga"
-import FullStory from "react-fullstory"
 import { initializeGoogleAds } from "../utils/googleAds"
 
 if (typeof window !== "undefined") {
@@ -18,7 +17,6 @@ class CustomApp extends App {
     return (
       <>
         <Component {...pageProps} />
-        {process.browser && window.location.hostname === "outgrow.io" && <FullStory key="fullstory" org="J1X09" />}
       </>
     )
   }
